9b9979bf3e1e1df47e5d7b14f509dde95006b902
Alexander Sharov <kvendingoldo@yandex.ru>
2025-05-29 11:37:44 +0400

    feat: commit changes

--- src/v2.py
+++ src/v2.py
@@ -34,8 +34,7 @@ def get_config():
         "github": {
             "url": os.getenv("GITHUB_API_URL", "https://api.github.com"),
             "repository": os.getenv("GITHUB_REPOSITORY"),
-            "token": os.environ.get("INPUT_GITHUB_TOKEN"),
-            "releases_uri": "/repos/{repo}/releases"
+            "token": os.environ.get("INPUT_GITHUB_TOKEN")
         },
         "features": {
 
@@ -47,12 +46,32 @@ def get_config():
     return config
 
 
-def bump():
-    pass
 
+def create_github_release(config, tag):
+    release_data = {
+        "name": tag,
+        "tag_name": tag,
+        "draft": False,
+        "prerelease": False,
+        "body": "",
+        "generate_release_notes": False
+    }
 
-def create_github_release(tag):
-    pass
+    logging.info(f"Creating GitHub release {release_data['name']}")
+
+    url = f"{config['github']['url']}/repos/{config['github']['repository']}/releases"
+
+    headers = {
+        "Authorization": f"Bearer {config['github']['token']}"
+    }
+    resp = requests.post(
+        url, json=release_data, headers=headers, timeout=60
+    )
+
+    if not resp:
+        print(f"text: {resp.text!r}")
+        resp.raise_for_status()
+
 
 def get_bump_type(commit_message):
     result = 'minor'
@@ -62,27 +81,32 @@ def get_bump_type(commit_message):
     #
     major_bump_keywords = ['[BUMP-MAJOR]', 'bump-major', 'feat!']
     if any(keyword in commit_message for keyword in major_bump_keywords):
-        result= 'major'
+        result = 'major'
 
     #
     # patch
     #
     patch_bump_keywords = ['[hotfix]', '[fix]', 'hotfix:', 'fix:']
     if any(keyword.lower() in commit_message.lower() for keyword in patch_bump_keywords):
-        result= 'patch'
+        result = 'patch'
 
     logging.info(f'Based on the commit message {commit_message} {result} bump is required')
     return result
 
-def get_semver_version(git_tag=None):
 
+def get_semver_version(config, git_tag=None):
     if git_tag is None:
-        return semver.VersionInfo.parse("0.0.0")
+        return semver.VersionInfo.parse(config["initial_version"])
 
-    return semver.VersionInfo.parse(git_tag.replace("rc/", "").replace("v", ""))
+    git_tag_without_prefixes = git_tag
+    for tag_prefix in config["tag_prefix"]:
+        git_tag_without_prefixes.replace(tag_prefix, "")
 
-def get_new_semver_version(tag_last, bump_type):
-    version = get_semver_version(tag_last)
+    return semver.VersionInfo.parse(git_tag_without_prefixes)
+
+
+def get_new_semver_version(config, tag_last, bump_type):
+    version = get_semver_version(config, tag_last)
 
     if bump_type == 'patch':
         return version.bump_patch()
@@ -92,38 +116,34 @@ def get_new_semver_version(tag_last, bump_type):
         return version.bump_major()  # patch and minor are reset automatically
 
 
-
-
-
-
-def get_new_version(active_branch, commit_message, tag_head, tag_last):
-    print(active_branch)
-    print(commit_message)
-    print(tag_last)
-    print(tag_head)
-
-    if tag_head is not None:
-        return None
-
-    if active_branch == "main":
-        bump_type = get_bump_type(commit_message)
-        version = get_semver_version(tag_last)
-        bumped_version = get_bumped_version(version, bump_type)
-        print(bump_type)
-        print(version)
-        print(bumped_version)
-        # Get version tag_last
-
-    # bump_type = get_bump_type(base_version, branch, commit_message, last_tag, tag_for_head)
-    # if bump_type == 'patch':
-    #     return base_version.bump_patch()
-    # if bump_type == 'minor':
-    #     return base_version.bump_minor()  # patch is reset automatically
-    # if bump_type == 'major':
-    #     return base_version.bump_major()  # patch and minor are reset automatically
-    # logging.info('No bump requested returning original version %s', base_version)
-    # return base_version
-    return ''
+# def get_new_version(active_branch, commit_message, tag_head, tag_last):
+#     print(active_branch)
+#     print(commit_message)
+#     print(tag_last)
+#     print(tag_head)
+#
+#     if tag_head is not None:
+#         return None
+#
+#     if active_branch == "main":
+#         bump_type = get_bump_type(commit_message)
+#         version = get_semver_version(tag_last)
+#         bumped_version = get_bumped_version(version, bump_type)
+#         print(bump_type)
+#         print(version)
+#         print(bumped_version)
+#         # Get version tag_last
+#
+#     # bump_type = get_bump_type(base_version, branch, commit_message, last_tag, tag_for_head)
+#     # if bump_type == 'patch':
+#     #     return base_version.bump_patch()
+#     # if bump_type == 'minor':
+#     #     return base_version.bump_minor()  # patch is reset automatically
+#     # if bump_type == 'major':
+#     #     return base_version.bump_major()  # patch and minor are reset automatically
+#     # logging.info('No bump requested returning original version %s', base_version)
+#     # return base_version
+#     return ''
 
 
 def create_release_branch(repo, new_version):
@@ -135,6 +155,7 @@ def create_release_branch(repo, new_version):
         logging.info("Release branch %s successfully pushed", release_branch)
     except Exception as ex:
         logging.info("Failed to create release branch %s. Error: %s", release_branch, ex)
+
 
 def push_git_tag(repo, tag):
     print("pushed")
@@ -193,42 +214,39 @@ def main():
     if tag_head is None:
         logging.info("There is no tag for HEAD")
 
-
-
     if active_branch == config["primary_branch"]:
         bump_type = get_bump_type(commit_message)
 
         #
         # Calculate new version
-        new_semver_version = get_new_semver_version(tag_last, bump_type)
+        new_semver_version = get_new_semver_version(config, tag_last, bump_type)
         new_tag = f"{config['tag_prefix']['candidate']}{str(new_semver_version)}"
         logging.info(f"New tag: {new_tag}")
         push_git_tag(repo, new_tag)
 
-        if (active_branch in config["auto_release_branches"]) or ('[RELEASE]' in commit_message and active_branch == config["primary_branch"]):
+        if (active_branch in config["auto_release_branches"]) or (
+            '[RELEASE]' in commit_message and active_branch == config["primary_branch"]):
             logging.info("Create new release")
             create_release_branch(repo, new_semver_version)
 
             #
             # Create GitHub release
-            create_github_release(new_tag)
+            create_github_release(config, new_tag)
 
             #
             # Switch back, and bump version in primary branch
             repo.git.checkout(active_branch)
 
-
-
             new_semver_version_after_release = new_semver_version.bump_minor()
             new_tag = f"{config['tag_prefix']['candidate']}{str(new_semver_version_after_release)}"
             logging.info(f"New tag for primary branch: {new_tag}")
 
-            repo.git.commit('--allow-empty', '-m', f"[semver-action] Bump upsteam version up to {str(new_semver_version_after_release)}")
+            repo.git.commit('--allow-empty', '-m',
+                            f"[semver-action] Bump upsteam version up to {str(new_semver_version_after_release)}")
             origin = repo.remote(name='origin')
             origin.push()
 
             push_git_tag(repo, new_tag)
-
 
     if active_branch.startswith("release/"):
         #
@@ -243,14 +261,14 @@ def main():
 
         #
         # Calculate new version
-        new_semver_version = get_new_semver_version(tag_last, bump_type)
+        new_semver_version = get_new_semver_version(config, tag_last, bump_type)
         new_tag = f"{config['tag_prefix']['release']}{str(new_semver_version)}"
         logging.info(f"New tag: {new_tag}")
         push_git_tag(repo, new_tag)
 
         #
         # Create GitHub release
-        create_github_release(new_tag)
+        create_github_release(config, new_tag)
 
     custom_branch = False
     if custom_branch:
